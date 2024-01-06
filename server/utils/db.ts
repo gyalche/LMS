35c2554335903b10ee32418ca0348392d93a15b9
dawa sherpa <sherpadawag1998@gmail.com>
2024-01-06 15:57:04 +0545

    working on backend setup

---
+++ server/utils/db.ts
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const dbUrl: string = process.env.DB_URI || '';
+
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(dbUrl).then((data: any) => {
+      console.log('database is connected with' + data.connection.host);
+    });
+  } catch (error) {
+    console.log(error.message);
+    setTimeout(connectDB, 5000);
+  }
+};
